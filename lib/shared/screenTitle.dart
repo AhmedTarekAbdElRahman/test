b7931b0aebedc3920181ae856cc558611a5e270c
Ahmed Tarek <ahmedtarekabdelrahman1@gmail.com>
2023-02-10 02:14:25 +0200

    test commit

---
+++ lib/shared/screenTitle.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class ScreenTitle extends StatelessWidget {
+  final String text;
+
+  const ScreenTitle({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TweenAnimationBuilder(
+      curve:Curves.easeIn,
+      duration: const Duration(milliseconds: 500),
+      tween: Tween<double>(begin: 0, end: 1),
+      builder: (context, value, child) {
+        return Opacity(
+          opacity: value,
+          child: Padding(
+            padding: EdgeInsets.only(top: value * 20),
+            child: child,
+          ),
+        );
+      },
+      child: Text(
+        text,
+        style: const TextStyle(
+            fontSize: 36, color: Colors.white, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
